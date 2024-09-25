0bfb615de64ec2357bce17d8e54bc45161c04e3e
atlasblue <92083755+atlasblue@users.noreply.github.com>
2024-09-25 16:51:08 +0200

    Update move-erp.py

--- move-erp.py
+++ move-erp.py
@@ -2,8 +2,8 @@ import ntnx_networking_py_client
 
 # Configure the client
 config = ntnx_networking_py_client.Configuration()
-# IPv4/IPv6 address or FQDN of the cluster
-config.host = "10.55.79.39"
+# IPv4/IPv6 address or FQDN of Prism Central
+config.host = ""
 # Port to which to connect to
 config.port = 9440
 # Max retry attempts while reconnecting on a loss of connection
@@ -13,7 +13,7 @@ config.backoff_factor = 3
 # UserName to connect to the cluster
 config.username = "admin"
 # Password to connect to the cluster
-config.password = "Prior7*Swarm*Dingy"
+config.password = ""
 #disable ssl certificate check
 config.verify_ssl = False
 
@@ -22,8 +22,8 @@ client = ntnx_networking_py_client.ApiClient(configuration=config)
 vpcs_api = ntnx_networking_py_client.VpcsApi(api_client=client)
 vpc_pri = ntnx_networking_py_client.Vpc()
 vpc_dr = ntnx_networking_py_client.Vpc()
-ext_id_pri = "3bc5985d-7087-4538-ac4f-120a3f29242c" # VPC Primary
-ext_id_dr = "e0e444dd-a5e9-4d60-93b4-214a8b1f95bb" # VPC DR
+ext_id_pri = "3bc5985d-7087-4538-ac4f-120a3f29242c" # VPC Primary UUID
+ext_id_dr = "e0e444dd-a5e9-4d60-93b4-214a8b1f95bb" # VPC DR UUID
 api_reponse_pri = vpcs_api.get_vpc_by_id(extId=ext_id_pri)
 api_reponse_dr = vpcs_api.get_vpc_by_id(extId=ext_id_dr)
 
